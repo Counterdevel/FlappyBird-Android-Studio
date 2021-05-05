1df332b8053546261b0e2c6fa91ebdee34bc10f1
Pierre Muller <pierreeitel@hotmail.com>
2021-05-05 00:07:57 -0300

    Parte 1
    Aula 30/04/2021

--- core/src/com/flappybird/game/Game.java
+++ core/src/com/flappybird/game/Game.java
@@ -1,31 +1,55 @@
 package com.flappybird.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	//C O N S T R U Ç Ã O
+	private SpriteBatch batch;																		//Variavel para textura
+	private Texture passaro;																		//Variavel de textura para o passaro
+	private Texture fundo;																			//Variavel de textura para o fundo
+
+	//M O V I M E N T A
+	private int movimentaY = 0;																		//Variavel para movimento no eixo Y
+	private int movimentaX = 0;																		//Variavel para movimento no eixo X
+
+	//T E L A
+	private float larguraDis;																		//Variavel da largura do dispositivo
+	private float alturaDis;																		//Variavel da altura do dispositivo
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		batch = new SpriteBatch();																	//construção do batch
+		passaro = new Texture("passaro1.png");											//Aplica textura para o passaro
+		fundo = new Texture("fundo.png");												//Aplica textura para o fundo
+
+		larguraDis = Gdx.graphics.getWidth();														//Pega a largura do dispositivo
+		alturaDis = Gdx.graphics.getHeight();														//Pega a altura do dispositivo
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		batch.begin();																				//Inicializa a renderização
+
+		batch.draw(fundo,0,0, larguraDis, alturaDis);										//Renderiza o conteudo
+		batch.draw(passaro, 50,50, movimentaX, movimentaY);									//Renderiza o passaro
+
+		movimentaX++;
+		movimentaY++;
+
+		batch.end();																				//Termina a renderização
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
